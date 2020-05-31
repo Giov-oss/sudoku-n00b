737c4de2aa3806012c51d363970a33a7bf7b7a56
Giov-oss <amputee@live.cl>
2020-05-31 01:24:19 -0400

    Sudoku v1.2

--- sudoku.py
+++ sudoku.py
@@ -5,43 +5,16 @@ if __name__ == '__main__':
 	for _ in range(9):
 		tablero.append([int(i) for i in input().split()])
 
-tablero_incial=tablero
-
-def transponer_tablero():
-	global tablero_c
-	tablero_c = list(map(list, zip(*tablero)))
-
-def get_celda(l,m):
+def get_celda(x,y):
 	global celda
-	celda = []
-	celdas=([[tablero[i+3*h][j+3*k] for i in range(3) for j in range(3)]for h in range(3) for k in range(3)])
-	if l<3:
-		if m<3:
-			celda = celdas[0]
-		elif m>2 and m<6:
-			celda = celdas[1]
-		else:
-			celda = celdas[2]
-	elif l>2 and l<6:
-		if m<3:
-			celda = celdas[3]
-		elif m>2 and m<6:
-			celda = celdas[4]
-		else:
-			celda = celdas[5]
-	else: 
-		if m<3:
-			celda = celdas[6]
-		elif m>2 and m<6:
-			celda = celdas[7]
-		else:
-			celda = celdas[8]
+	l=(x//3)*3
+	m=(y//3)*3
+	celda=[tablero[l+i][m+j] for i in range(3) for j in range(3)]
 
 def possible(i,j,n):
-	transponer_tablero()
 	get_celda(i,j)
 	fila = tablero[i]
-	columna = tablero_c[j]
+	columna = [tablero[k][j] for k in range(9)]
 	if fila.count(n) == 0 and columna.count(n) == 0 and celda.count(n)==0:
 		return True
 
@@ -63,9 +36,3 @@ rastreo()
 elapsed_time = time.process_time() - t
 
 print(elapsed_time)
-
-
-
-
-
-
